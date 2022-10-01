41021cf13122a392070b6889de6fbc5e1f7fa723
ThatHypedPerson <thathypedperson@gmail.com>
2022-10-01 06:22:09 -0700

    Remove excess comments. they just contained deconstructed data of how jchat handles requests hopefully i remember to look through commit history to find them if i ever need the data again

--- v2/youtube.js
+++ v2/youtube.js
@@ -127,7 +127,8 @@ function hashFnv32a(str, asString, seed) { // fixes a bug when deleting a messag
     return hval >>> 0;
 }
 
-const message_wait = 10000 // wait 5s between requests
+const message_wait = 5000 // wait 5s between requests
+// const message_wait = 10000 // wait 10s between requests
 async function execute() {
 	if(typeof gapi.client != "undefined")
 	{
@@ -150,78 +151,3 @@ async function execute() {
 		setTimeout(execute, message_wait)
 	}
 }
-
-// nick
-// thathypedperson
-// set as authorDetails["channelId"]
-
-// info
-// {
-// 	"badge-info": "subscriber/8", (test with different accounts)
-// 	badges: "broadcaster/1,subscriber/0,glhf-pledge/1", (only check for listed below)
-// 	color: "#8A2BE2", (ignore?) (true?)
-// 	"display-name": "ThatHypedPerson", (authorDetails["displayName"])
-// 	emotes: true,
-// 	"first-msg": "0",
-// 	flags: true,
-// 	id: "ef2b4afe-1038-4cd3-b7f5-51bb024bbcf5", (items["id"])
-// 	mod: "0", (authorDetails["isChatModerator"])
-// 	"returning-chatter": "0",
-// 	"room-id": "133875470",
-// 	subscriber: "1",
-// 	"tmi-sent-ts": "1664614269893", (epoch time)
-// 	turbo: "0",
-// 	"user-id": "133875470", (authorDetails["channelId"]?)
-// 	"user-type": true
-// }
-// 
-// mod
-// {
-// 	"badge-info": true,
-// 	badges: "moderator/1",
-// 	color: true,
-// 	"display-name": "ddepyh",
-// 	emotes: true,
-// 	"first-msg": "0",
-// 	flags: true,
-// 	id: "b23fdc56-f5d2-4bf8-94f7-0dd168483077",
-// 	mod: "1",
-// 	"returning-chatter": "0",
-// 	"room-id": "133875470",
-// 	subscriber: "0",
-// 	"tmi-sent-ts": "1664615202719",
-// 	turbo: "0",
-// 	"user-id": "615959677",
-// 	"user-type": "mod"
-// }
-// 
-// regular
-// {
-// 	"badge-info": true,
-// 	badges: true,
-// 	color: true,
-// 	"display-name": "ddepyh",
-// 	emotes: true,
-// 	"first-msg": "0",
-// 	flags: true,
-// 	id: "11fa175e-a0fd-4340-8ea3-6c395caff78a",
-// 	mod: "0",
-// 	"returning-chatter": "0",
-// 	"room-id": "133875470",
-// 	subscriber: "0",
-// 	"tmi-sent-ts": "1664615520811",
-// 	turbo: "0",
-// 	"user-id": "615959677",
-// 	"user-type": true
-// }
-
-// message
-// chat message
-// snippet["textMessageDetails"]["messageText"]
-
-// "broadcaster:1 (streamer) (authorDetails["isChatOwner"])
-// "moderator:1" (moderator) (authorDetails["isChatModerator"])
-// "partner:1" (verified channels?) (authorDetails["isVerified"])
-// "subscriber:0" (sponsors [i wish], last number is months) (authorDetails["isChatSponsor"])
-
-// send(nick, info, message)
